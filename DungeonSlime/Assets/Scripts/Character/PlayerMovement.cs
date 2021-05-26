bc0b1f1c952fe805d8e8b247ca237fc14cb902fb
sidnei <sidnei@opalastudios.com>
2021-05-25 22:46:22 -0300

    hologram sprite + raycast updated  but not working fine

--- DungeonSlime/Assets/Scripts/Character/PlayerMovement.cs
+++ DungeonSlime/Assets/Scripts/Character/PlayerMovement.cs
@@ -9,6 +9,12 @@ using UnityEngine;
 
 namespace DungeonSlime.Character {
     public class PlayerMovement : MonoBehaviour {
+        
+        private float m_rayLenght = 50f;
+        private float m_raySpacing = .35f;
+        private int m_verticalRayCount;
+        private int m_horizontalRayCount;
+        private float m_oldDistance = 500f;
 
         [SerializeField] private float m_speed;
         [SerializeField] private Transform m_hit_right_up;
@@ -16,74 +22,83 @@ namespace DungeonSlime.Character {
         [SerializeField] private Transform m_hit_left_up;
         [SerializeField] private Transform m_hit_left_down;
         [SerializeField] private LayerMask m_groundMask;
-        [SerializeField] private BoxCollider2D m_collider;
+        [SerializeField] private SpriteRenderer m_sprite;
         public float OffsetDistance;
         public Ease EaseMovement;
+ 
+        private void Start() {
+            m_verticalRayCount = Mathf.RoundToInt(m_sprite.bounds.size.y / .25f);
+            m_horizontalRayCount = Mathf.RoundToInt(m_sprite.bounds.size.x / .25f);
+        }
 
         private void Update() {
+            
             if (Input.GetKeyDown(KeyCode.D)) {
-                transform.DOMoveX(
-                    evaluateRaycast(m_hit_right_up.transform.position, m_hit_right_down, Vector2.right, Vector2.down).transform.position
-                        .x -
-                    OffsetDistance,
-                    m_speed).SetEase(EaseMovement).OnComplete(() => {
-                    GameManager.Instance.GlobalDispatcher.Emit(new OnUpdateSlimeForm());
-                });
+//                transform.DOMoveX(
+//                    evaluateRaycast(m_hit_right_up.transform.position, m_verticalRayCount, Vector2.right, Vector2.down).transform.position
+//                        .x -
+//                    OffsetDistance,
+//                    m_speed).SetEase(EaseMovement).OnComplete(() => {
+//                    GameManager.Instance.GlobalDispatcher.Emit(new OnUpdateSlimeForm());
+//                });
             }
 
             if (Input.GetKeyDown(KeyCode.A)) {
-                transform.DOMoveX(
-                    evaluateRaycast(m_hit_left_up.transform.position, m_hit_left_down, Vector2.left, Vector2.down).transform.position.x +
-                    OffsetDistance,
-                    m_speed).SetEase(EaseMovement);
+//                transform.DOMoveX(
+//                    evaluateRaycast(m_hit_left_up.transform.position, m_verticalRayCount, Vector2.left, Vector2.down).transform.position.x +
+//                    OffsetDistance,
+//                    m_speed).SetEase(EaseMovement);
             }
 
             if (Input.GetKeyDown(KeyCode.W)) {
-                transform.DOMoveY(
-                    evaluateRaycast(m_hit_right_up.transform.position, m_hit_left_up, Vector2.up, Vector2.left).transform.position.y -
-                    OffsetDistance,
-                    m_speed).SetEase(EaseMovement);
+//                transform.DOMoveY(
+//                    evaluateRaycast(m_hit_right_up.transform.position, m_horizontalRayCount, Vector2.up, Vector2.left).transform.position.y -
+//                    OffsetDistance,
+//                    m_speed).SetEase(EaseMovement);
             }
 
             if (Input.GetKeyDown(KeyCode.S)) {
-                transform.DOMoveY(
-                    evaluateRaycast(m_hit_right_down.transform.position, m_hit_left_down, Vector2.down, Vector2.left).transform.position
-                        .y +
-                    OffsetDistance,
-                    m_speed).SetEase(EaseMovement);
+//                transform.DOMoveY(
+//                    evaluateRaycast(m_hit_right_down.transform.position, m_horizontalRayCount, Vector2.down, Vector2.left).transform.position
+//                        .y +
+//                    OffsetDistance,
+//                    m_speed).SetEase(EaseMovement);
             }
 
-            for (int i = 0; i < 5; i++) {
-                Debug.DrawRay((Vector2)m_hit_right_up.transform.position + 0.35f * i * Vector2.down, Vector2.right, Color.red);
-            }
+//            var value = Mathf.RoundToInt(m_sprite.bounds.size.y / .35f);
+//            for (var i = 0; i < value; i++) {
+//                Debug.DrawRay((Vector2)m_hit_left_up.transform.position + m_raySpacing* i * Vector2.down, Vector2.left, Color.red);
+//            }
+//            
+//            var value2 = Mathf.RoundToInt(m_sprite.bounds.size.x / .35f);
+//            for (var i = 0; i < value2; i++) {
+//                Debug.DrawRay((Vector2)m_hit_right_up.transform.position + m_raySpacing* i * Vector2.left, Vector2.up, Color.blue);
+//            }
         }
 
-        private RaycastHit2D evaluateRaycast(Vector2 originHit, Transform secondHit, Vector2 rayDirection,
+        private RaycastHit2D evaluateRaycast(Vector2 originHit, int rayCount, Vector2 rayDirection,
             Vector2 raySense) {
-            var rayLenght = 50f;
-            var raySpacing = .35f;
-            var rayCount = 5; //maybe change this value based on slime form?
-            var oldDistance = 500f;
+
+            m_oldDistance = 500f;
             var minorHitDistance = new RaycastHit2D();
-            Vector2 rayOrigin = originHit;
             
             for (var i = 0; i < rayCount; i++) {
-                var hit = Physics2D.Raycast(rayOrigin + raySpacing * i * raySense, rayDirection, rayLenght, m_groundMask);
-            
-                Debug.DrawRay(rayOrigin + raySpacing * i * raySense, rayDirection, Color.red);
+                var hit = Physics2D.Raycast(originHit + m_raySpacing * i * raySense, rayDirection, m_rayLenght, m_groundMask);
+
+                Debug.DrawRay(originHit + m_raySpacing * i * raySense, rayDirection, Color.red);
                
                 if (hit) {
-                    //Debug.Log("bati no: " + hit.collider.name);
+                    Debug.Log("bati no: " + hit.collider.name);
                     var minorDistance = (Vector3)originHit - hit.transform.position;
 
-                    if (minorDistance.magnitude < oldDistance) {
-                        oldDistance = minorDistance.magnitude;
+                    if (minorDistance.magnitude < m_oldDistance) {
+                        m_oldDistance = minorDistance.magnitude;
                         minorHitDistance = hit;
-                        //Debug.Log("menor: " + hit.collider.name);
+                        Debug.Log("menor: " + hit.collider.name);
                     }
                 }
             }
-
+            
             return minorHitDistance;
         }
     }
