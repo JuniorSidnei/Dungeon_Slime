a2b79e1ace209d1108f923cc7a5258477c9a9173
sidnei <sidnei@opalastudios.com>
2021-05-27 21:18:23 -0300

    collision event

--- DungeonSlime/Assets/Scripts/Utils/GlobalEvents.cs
+++ DungeonSlime/Assets/Scripts/Utils/GlobalEvents.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 namespace DungeonSlime.Utils {
 
     public class OnUpdateSlimeForm {  }
+
+    public class OnCollisionDetected {
+        public OnCollisionDetected(int objectLayer, Vector2 currentDirection) {
+            ObjectLayer = objectLayer;
+            CurrentDirection = currentDirection;
+        }
+
+        public int ObjectLayer;
+        public Vector2 CurrentDirection;
+    }
 }
