2f22a0894e708368a9240699c2e77703df881ead
sidnei <sidnei@opalastudios.com>
2021-05-30 22:19:46 -0300

    game manager uses level manager

--- DungeonSlime/Assets/Scripts/Managers/GameManager.cs
+++ DungeonSlime/Assets/Scripts/Managers/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using GameToBeNamed.Utils;
@@ -7,6 +8,11 @@ namespace DungeonSlime.Utils {
     public class GameManager : Singleton<GameManager> {
 
         public QueuedEventDispatcher GlobalDispatcher = new QueuedEventDispatcher();
+        public LevelManager m_levelManager;
+
+        private void Start() {
+            m_levelManager.LoadLevel(0);
+        }
 
         private void Update() {
             GlobalDispatcher.DispatchAll();
