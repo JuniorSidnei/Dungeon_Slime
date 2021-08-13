f4a32ae76c7ec609090b19682fb44ff4548dadb1
sidnei <sidnei@opalastudios.com>
2021-08-12 22:05:26 -0300

    meta files and removed stuffs

--- DungeonSlime/Assets/Scripts/Managers/GameManager.cs
+++ DungeonSlime/Assets/Scripts/Managers/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using GameToBeNamed.Utils;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace DungeonSlime.Managers {
     public class GameManager : Singleton<GameManager> {
@@ -10,12 +11,14 @@ namespace DungeonSlime.Managers {
         public QueuedEventDispatcher GlobalDispatcher = new QueuedEventDispatcher();
         public LevelManager m_levelManager;
 
-        private void Start() {
-            m_levelManager.LoadLevel(0);
-        }
-
+        
         private void Update() {
             GlobalDispatcher.DispatchAll();
+        }
+
+        public void LoadNextScene(int nextLevelIndex) {
+            //TODO:: make async operation with transition and loading scene
+            SceneManager.LoadScene(string.Format("Level_0{0}", nextLevelIndex));
         }
     }
 }
