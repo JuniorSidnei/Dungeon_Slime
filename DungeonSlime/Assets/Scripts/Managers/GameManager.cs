428fc484cf6ab99def795bdb2a57f8594f3f78e2
sidnei <sidnei@opalastudios.com>
2021-08-17 21:38:04 -0300

    load async next scene

--- DungeonSlime/Assets/Scripts/Managers/GameManager.cs
+++ DungeonSlime/Assets/Scripts/Managers/GameManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DungeonSlime.Scriptables;
+using DungeonSlime.Utils;
 using GameToBeNamed.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,16 +11,34 @@ namespace DungeonSlime.Managers {
     public class GameManager : Singleton<GameManager> {
 
         public QueuedEventDispatcher GlobalDispatcher = new QueuedEventDispatcher();
-        public LevelManager m_levelManager;
+        public DoTransitionController transitionController;
+        private AsyncOperation m_loadScene;
 
+        private void OnEnable()  {
+            StartCoroutine(WaitToFadeOut(2));
+        }
         
         private void Update() {
             GlobalDispatcher.DispatchAll();
         }
-
+        
         public void LoadNextScene(int nextLevelIndex) {
-            //TODO:: make async operation with transition and loading scene
-            SceneManager.LoadScene(string.Format("Level_0{0}", nextLevelIndex));
+            transitionController.DoTransitionIn(0.5f, () => {
+                StartCoroutine(LoadScene(string.Format("Level_0{0}", nextLevelIndex)));    
+            });
+        }
+
+        IEnumerator WaitToFadeOut(float time) {
+            yield return new WaitForSeconds(time);
+            transitionController.DoTransitionOut(2f);
+        }
+        
+        IEnumerator LoadScene(string sceneToLoad) {
+            m_loadScene = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Single);
+            
+            while (!m_loadScene.isDone) {
+                yield return null;
+            }
         }
     }
 }
