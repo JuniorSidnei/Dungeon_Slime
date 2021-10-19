e7723e3f2c92e8efa900c764c41ac9c58788ead6
sidnei <sidnei@opalastudios.com>
2021-10-19 19:08:21 -0300

    added lvl 15

--- DungeonSlime/Assets/Scripts/EnviromentObjects/RockStates.cs
+++ DungeonSlime/Assets/Scripts/EnviromentObjects/RockStates.cs
@@ -11,11 +11,11 @@ namespace DungeonSlime.Enviroment {
         
         [SerializeField] private Vector2Int m_initialPosition;
 
-        private Dictionary <CharacterForms, Vector2Int> m_slotsOnGrid = new Dictionary<CharacterForms, Vector2Int> {
+        private readonly Dictionary <CharacterForms, Vector2Int> m_slotsOnGrid = new Dictionary<CharacterForms, Vector2Int> {
             {CharacterForms.NORMAL, new Vector2Int(2, 2)},
         };
 
-        private Dictionary<Vector2, Vector2Int> m_collisionOffsetPosition = new Dictionary<Vector2, Vector2Int> {
+        private readonly Dictionary<Vector2, Vector2Int> m_collisionOffsetPosition = new Dictionary<Vector2, Vector2Int> {
             {Vector2.down, new Vector2Int(0, 1)},
             {Vector2.up, new Vector2Int(0, 0)},
             {Vector2.left, new Vector2Int(1, 0)},
