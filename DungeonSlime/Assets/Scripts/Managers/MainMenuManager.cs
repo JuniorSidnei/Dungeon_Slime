37af544cb97c8062e70e6b06dd86c85f74dcf423
sidao <sidneicarrarojunio@gmail.com>
2021-11-17 18:41:11 -0300

    added 2d light +  main menu finished

--- DungeonSlime/Assets/Scripts/Managers/MainMenuManager.cs
+++ DungeonSlime/Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,5 +1,7 @@
 using DungeonSlime.Utils;
 using GameToBeNamed.Utils;
+using GameToBeNamed.Utils.Sound;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -20,50 +22,77 @@ namespace DungeonSlime.Managers {
         public GameObject controlBtn;
         public GameObject optionsBtn;
         public GameObject quitBtn;
-        
+
+        [Header("sounds")]
+        public AudioClip menuTheme;
+        public AudioClip selection;
+        public AudioClip choose;
+
         private void Start() {
             if (!firstSelected) return;
             
             firstSelected.Select();
+            AudioController.Instance.Play(menuTheme, AudioController.SoundType.Music, 0.25f, true);
         }
 
         public void OnPlaySelected() {
             AnimateSlimeSelector();
+            playBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.yellow;
             slimeSelector.transform.position = new Vector3(slimeSelector.transform.position.x, playBtn.transform.position.y, 0);
         }
-
+        
+        public void OnPlayDeselected() {
+            playBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+        }
+        
         public void OnControlSelected() {
             AnimateSlimeSelector();
             slimeSelector.transform.position = new Vector3(slimeSelector.transform.position.x, controlBtn.transform.position.y, 0);
         }
-
+        
+        public void OnControlDeselected() {
+            controlBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+        }
+        
         public void OnOptionsSelected() {
             AnimateSlimeSelector();
             slimeSelector.transform.position = new Vector3(slimeSelector.transform.position.x, optionsBtn.transform.position.y, 0);
         }
-
+        public void OnOptionsDeselected() {
+            optionsBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+        }
+        
         public void OnQuitSelected() {
             AnimateSlimeSelector();
             slimeSelector.transform.position = new Vector3(slimeSelector.transform.position.x, quitBtn.transform.position.y, 0);
         }
         
+        public void OnQuitDeselected() {
+            quitBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+        }
+        
         public void OnPlayPressed() {
+            AudioController.Instance.Play(choose, AudioController.SoundType.SoundEffect2D);
             SceneManager.LoadScene("LevelDifficultySelectionMenu");
         }
-
+        
         public void OnControlPressed() {
+            AudioController.Instance.Play(choose, AudioController.SoundType.SoundEffect2D);
             Debug.Log("apertei control");
         }
-
+        
         public void OnOptionsPressed() {
+            AudioController.Instance.Play(choose, AudioController.SoundType.SoundEffect2D);
             SceneManager.LoadScene("OptionsMenu");
         }
-
+        
         public void OnQuitPressed() {
+            AudioController.Instance.Play(choose, AudioController.SoundType.SoundEffect2D);
             Application.Quit();
         }
-
-        private void AnimateSlimeSelector()  {
+        
+        private void AnimateSlimeSelector() {
+            AudioController.Instance.Play(selection, AudioController.SoundType.SoundEffect2D);
             slimeSelectorAnim.SetTrigger("move");
         }
      }
