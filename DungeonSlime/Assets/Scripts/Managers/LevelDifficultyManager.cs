afafde9d9e5bf20717c0548fa5cdf9adbc59c9db
sidao <sidneicarrarojunio@gmail.com>
2021-11-18 15:26:17 -0300

    level dificulty completed

--- DungeonSlime/Assets/Scripts/Managers/LevelDifficultyManager.cs
+++ DungeonSlime/Assets/Scripts/Managers/LevelDifficultyManager.cs
@@ -1,4 +1,5 @@
 using DungeonSlime.Utils;
+using GameToBeNamed.Utils.Sound;
 using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine;
@@ -10,25 +11,47 @@ namespace DungeonSlime.Managers {
     public class LevelDifficultyManager : MonoBehaviour {
         
         public TextMeshProUGUI adviseTxt;
-        public RectTransform slimeSelector;
-        public RectTransform normalBtn;
-        public RectTransform hardBtn;
+        
+        [Header("slime selector")]
+        public GameObject slimeSelector;
+        public Animator slimeSelectorAnim;
+        
+        [Header("buttons")]
+        public GameObject normalBtn;
+        public GameObject hardBtn;
 
+        [Header("audios")]
+        public AudioClip selection;
+        public AudioClip selectionDone;
+        public AudioClip selectionBack;
+        
         private enum LevelDifficulty {
             NORMAL = 0,
             HARD
         }
         
         public void OnNormalSelected() {
+            AnimateSlimeSelector();
+            normalBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.yellow;
             slimeSelector.transform.position = new Vector3(slimeSelector.transform.position.x, normalBtn.transform.position.y, 0);
             adviseTxt.gameObject.SetActive(false);
         }
 
+        public void OnNormalDeselected() {
+            normalBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+        }
+        
         public void OnHardSelected() {
+            AnimateSlimeSelector();
+            hardBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.yellow;
             slimeSelector.transform.position = new Vector3(slimeSelector.transform.position.x, hardBtn.transform.position.y, 0);
             adviseTxt.gameObject.SetActive(true);
         }
 
+        public void OnHardDeselected() {
+            hardBtn.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
+        }
+        
         public void OnNormalPressed() {
            SaveDifficultySelection(LevelDifficulty.NORMAL);
             SceneManager.LoadScene("LevelSelectionMenu");
@@ -41,6 +64,11 @@ namespace DungeonSlime.Managers {
 
         public void BackMenu() {
             SceneManager.LoadScene("MainMenu");
+        }
+        
+        private void AnimateSlimeSelector() {
+            AudioController.Instance.Play(selection, AudioController.SoundType.SoundEffect2D);
+            slimeSelectorAnim.SetTrigger("move");
         }
         
         private void SaveDifficultySelection(LevelDifficulty levelDifficulty) {
